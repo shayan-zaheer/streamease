b06f29ff23238a62897ac580c4872abee5d2b0f1
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-08-26 02:36:05 +0500

    Update PlayPage.jsx

--- client/src/pages/PlayPage.jsx
+++ client/src/pages/PlayPage.jsx
@@ -1,37 +1,63 @@
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import MoviePlay from "../components/MoviePlay";
+import { statusActions } from "../store/statusSlice";
 
-function PlayPage(){  
-    const [movie, setMovie] = useState("");
-    
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
+function PlayPage() {
+	const { fetching, fetchDone } = useSelector((store) => store.status);
+	const dispatch = useDispatch();
+	const [movie, setMovie] = useState("");
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
 		const movieId = urlParams.get("id");
 
-        const controller = new AbortController();
-        const signal = controller.signal;
+		if (fetchDone) {
+			return;
+		}
 
-        const fetchMovie = async() => {
-            if (movieId) {
-                const data = await fetch(`http://localhost:8000/movies/${movieId}`, {signal});
-                const response = await data.json();
-                setMovie(response.movie);
-            }
-        }
+		const controller = new AbortController();
+		const signal = controller.signal;
 
-        fetchMovie();
+		const fetchMovie = async () => {
+			if (movieId) {
+				try {
+					dispatch(statusActions.markFetchingStarted());
 
-        return () => {
-            controller.abort();
-        }
+					const data = await fetch(
+						`http://localhost:8000/movies/${movieId}`,
+						{ signal }
+					);
+					const response = await data.json();
+					setMovie(response.movie);
 
-    }, [])
+					dispatch(statusActions.markFetchDone());
+				} catch (err) {
+					console.error(err);
+				} finally {
+					dispatch(statusActions.markFetchingFinished());
+				}
+			}
+		};
 
-    return (
-        <div class="play-container container">
-			{movie && <MoviePlay movie={movie} />}
-		</div>
-    );
-};
+		fetchMovie();
+
+		return () => {
+			controller.abort();
+		};
+	}, [fetchDone, dispatch]);
+
+	return (
+		<>
+			{fetching || !fetchDone ? (
+				<LoadingSpinner />
+			) : (
+				<div class="play-container container">
+					{movie && <MoviePlay movie={movie} />}
+				</div>
+			)}
+		</>
+	);
+}
 
 export default PlayPage;
